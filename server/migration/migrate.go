7c1dcfcef6d1d0ba09b73b8c0714a5b45d45cc9a
nakamuraitsuki <isukin@gmail.com>
2025-01-10 09:04:50 +0900

    feat: add todo_lists table migration

--- server/migration/migrate.go
+++ server/migration/migrate.go
@@ -20,14 +20,25 @@ func Migrate(db *sqlx.DB) error {
 	}
 
 	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS todos (
+		CREATE TABLE IF NOT EXISTS todo_lists (
 			id INT PRIMARY KEY AUTO_INCREMENT,
 			user_id INT NOT NULL,
+			name VARCHAR(255) NOT NULL,
+			created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES users(id)
+		)
+	`)
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS todos (
+			id INT PRIMARY KEY AUTO_INCREMENT,
+			todo_list_id INT NOT NULL,
 			title VARCHAR(255) NOT NULL,
 			completed BOOLEAN NOT NULL DEFAULT FALSE,
 			created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-			FOREIGN KEY (user_id) REFERENCES users(id)
+			FOREIGN KEY (todo_list_id) REFERENCES todo_lists(id)
 		)
 	`)
 	if err != nil {
