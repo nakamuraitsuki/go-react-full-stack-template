a7a7282b4e4bfb30fed50f949a2aeeb10210dbff
nakamuraitsuki <isukin@gmail.com>
2025-01-11 12:41:29 +0900

    fix:更新忘れ

--- server/model/todo.go
+++ server/model/todo.go
@@ -4,8 +4,8 @@ import "time"
 
 type Todo struct {
 	ID          int       `db:"id"`
-	TodoListID  int       `db:"user_id"`
-	Title       string    `db:"title"`
+	TodoListID  int       `db:"todo_list_id"`
+	Title       string    `db:"title"`	
 	Completed   bool      `db:"completed"`
 	CreatedAt   time.Time `db:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at"`
